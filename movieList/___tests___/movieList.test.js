e0dbbadcd24ad5178e9f2fdbc9274db4f964e74b
Vadzim_Yesman <vvyesman@gmail.com>
2022-01-26 23:55:45 -0800

    added 3 more tests

--- movieList/___tests___/movieList.test.js
+++ movieList/___tests___/movieList.test.js
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 //Import the functions I want to test
-const {deleteMovie} = require("../functions/deleteMovie.js")
+const {deleteMovie, checkOffMovie, checkMessageText,deleteMovie2} = require("../functions/deleteMovie.js")
 
 beforeAll(async ()=>{
     await driver.get('http://localhost:5501/movieList/index.html')
@@ -16,7 +16,21 @@ afterAll(async ()=>{
     await driver.quit()
 })
 
+
 test("Delete a movie", async ()=>{
     await deleteMovie(driver);
     //add await .sleep() to display results for longer
+})
+
+test("Check off movie", async ()=>{
+    await checkOffMovie(driver);
+    //add await .sleep() to display results for longer
+})
+
+test("Check message text", async ()=>{
+    await checkMessageText(driver);
+})
+
+test("Delete movie test #2", async ()=>{
+    await deleteMovie2(driver);
 })
--- movieList/functions/deleteMovie.js
+++ movieList/functions/deleteMovie.js
@@ -10,23 +10,115 @@ const deleteMovie = async (driver) =>{
 
     await driver.sleep(1000)
 
-    await driver.findElement(By.xpath('//button[contains(text(),"x")]'))
+    await driver.findElement(By.xpath('//button[contains(text(),"x")]')).click()
 
     await driver.sleep(1000)
 
     let array = await driver.findElements(By.xpath('//button[contains(text(),"x")]'))
 
-    let movieList = await driver.findElement(By.xpath('//ul'))
+    //let movieList = await driver.findElement(By.xpath('//ul'))
 
-    let numberOfMovies = movieList.allChildElements.size()
 
-    //let numberOfMovies = await movieList.findElements(By.xpath("*"))*/
+    //let numberOfMovies = await movieList.findElements(By.xpath("*"))
     
     //console.log(numberOfMovies.size())
 
-    expect(numberOfMovies).toBe(1)
+    expect(array.length).toBe(0)
 }
 
+
+const checkOffMovie = async (driver) =>{
+
+    await driver.findElement(By.xpath('//input')).clear()
+
+    await driver.findElement(By.xpath('//input')).sendKeys(`The professional`)
+
+    await driver.sleep(1000)
+
+    await driver.findElement(By.xpath('//button')).click()
+
+    await driver.sleep(1000)
+
+    await driver.findElement(By.xpath('//span')).click()
+
+    await driver.sleep(1000)
+
+    let array = await driver.findElements(By.className('checked'))
+
+ 
+
+    expect(array.length).toBe(1)
+}
+
+const checkMessageText = async (driver) =>{
+
+    await driver.findElement(By.xpath('//input')).clear()
+
+    //await driver.findElement(By.xpath('//input')).sendKeys(`The Professional`)
+
+    //await driver.sleep(1000)
+
+    //await driver.findElement(By.xpath('//button')).click()
+
+    await driver.sleep(1000)
+
+    //await driver.findElement(By.xpath('//span')).click()
+
+    await driver.findElement(By.xpath('//span')).click()
+
+    const message = await driver.findElement(By.id('message')).getText()
+
+    await driver.sleep(1000)
+
+    //let array = await driver.findElements(By.className('checked'))
+
+ 
+
+    expect(message).toBe("The professional added back!")
+}
+
+const deleteMovie2 = async (driver) =>{
+
+    await driver.findElement(By.xpath('//input')).clear()
+
+    await driver.findElement(By.xpath('//button[contains(text(),"x")]')).click()
+
+    await driver.sleep(1000)
+
+    await driver.findElement(By.xpath('//input')).sendKeys("Ameli")
+
+    await driver.findElement(By.xpath('//button')).click()
+
+    await driver.sleep(1000)
+
+    await driver.findElement(By.xpath('//input')).sendKeys("Rap")
+
+    await driver.findElement(By.xpath('//button')).click()
+
+    await driver.findElement(By.xpath('//button[contains(text(),"x")]'))
+
+    let movieList = await driver.findElement(By.xpath('//ul'))
+
+    let childrenArray = await movieList.findElements(By.xpath("./child::*"))
+
+    await driver.sleep(1000)
+
+    //let array = await driver.findElements(By.xpath('//button[contains(text(),"x")]'))
+
+    
+
+
+    //let numberOfMovies = await movieList.findElements(By.xpath("*"))
+    
+    //console.log(numberOfMovies.size())
+
+    expect(childrenArray.length).toBe(2)
+}
+
+
 module.exports = {
-    deleteMovie
+    deleteMovie,
+    checkOffMovie,
+    checkMessageText,
+    deleteMovie2
 }
